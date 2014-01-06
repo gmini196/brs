535cfe0df7dc237e1a3b066d1b7ec49ca41dc63b
Dac Nguyen Minh <nguyen.minh.dac@framgia.com>
2014-01-06 12:01:11 +0700

    add static pages and router

---
+++ app/controllers/admin/users_controller.rb
@@ -0,0 +1,26 @@
+class Admin::UsersController < ApplicationController
+        def index
+        end
+        
+        def show
+        end
+
+        def new
+        end
+
+        def create
+        end
+
+        def edit
+        end
+
+        def update
+        end
+        
+        def destroy
+        end
+
+        private
+         def user_params
+         end
+end
