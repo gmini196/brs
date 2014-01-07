9dbfd80c7df219d7780ad9137cf1384a811df492
Nhat Truong Duc <truong.duc.nhat@framgia.com>
2014-01-07 15:26:27 +0700

    Update a book

--- app/controllers/admin/books_controller.rb
+++ app/controllers/admin/books_controller.rb
@@ -22,6 +22,20 @@ class Admin::BooksController < ApplicationController
 	    end
 	end
 
+	def edit
+		@book = Book.find params[:id]
+		@categories = Category.all
+	end
+
+	def update
+		@book = Book.find params[:id] 
+    if @book.update_attributes! book_params
+      flash[:success] = "Book updated"
+      redirect_to admin_books_path
+    else
+      render 'edit'
+    end
+	end
 
 	private
 
