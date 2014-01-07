15aac6188215cf1143c72fe38d2818a1a6bcce64
HaChan <hades.24791@gmail.com>
2014-01-07 11:18:49 +0700

    refs #TaskShowAllBookUser

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,10 +1,10 @@
 class BooksController < ApplicationController
   def index
-    @books = Book.paginate(page: params[:page], per_page: 8)
+    @books = Book.paginate page: params[:page], per_page: 8
   end
 
   def show
-
+    @book = Book.find params[:id]
   end
 
 
