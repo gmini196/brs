9cd7e2a92afee2264c02d008d70ee014962b0a1d
Nhat Truong Duc <truong.duc.nhat@framgia.com>
2014-01-08 15:34:36 +0700

    Mark reading book

--- app/controllers/readings_controller.rb
+++ app/controllers/readings_controller.rb
@@ -5,4 +5,13 @@ class ReadingsController < ApplicationController
                                                    per_page: 2
   end
 
+  def create
+    Reading.create reading_params
+  end
+
+  private 
+
+    def reading_params
+      params.require(:reading).permit(:user_id, :book_id)
+    end
 end
